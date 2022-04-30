dd144d673db6c6ae2f6b0668286c0e73102f57c2
mnaofficial <mna.official92@gmail.com>
2022-04-30 15:12:22 +0600

    index.css and some file  modify

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/home' element={<Home/>}></Route>
