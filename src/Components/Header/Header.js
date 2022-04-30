d88d9a8449dea8561af24857664f9421b625ba59
mnaofficial <mna.official92@gmail.com>
2022-04-30 19:13:12 +0600

    header setup

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,7 +3,27 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-            <h2>Header</h2>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <a href="#" className="navbar-brand">
+                        <img src="images/logo.svg" height="28" alt="CoolBrand"/>
+                    </a>
+                    <button type="button" className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarCollapse">
+                        <div className="navbar-nav">
+                            <a href="#" className="nav-item nav-link active">Home</a>
+                            <a href="#" className="nav-item nav-link">Profile</a>
+                            <a href="#" className="nav-item nav-link">Messages</a>
+                            <a href="#" className="nav-item nav-link disabled" tabindex="-1">Reports</a>
+                        </div>
+                        <div className="navbar-nav ms-auto">
+                            <a href="#" className="nav-item nav-link">Login</a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
         </div>
     );
 };
