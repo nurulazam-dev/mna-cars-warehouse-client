3b0889288935bf711a4d806a94562645f6ec2a6d
mnaofficial <mna.official92@gmail.com>
2022-05-01 00:38:51 +0600

    NotFound 404 page setup complete

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import notFound from '../../Images/NotFound/404.png'
 
 const NotFound = () => {
     return (
         <div>
-            <h1>4 0 4</h1>
+            <div>
+            <img className=' w-75' src={notFound} alt="" />
+            </div>
+            <Link to='/'>
+            <button className='btn btn-primary w-25 fs-5'>Go Home</button>
+            </Link>
         </div>
     );
 };
