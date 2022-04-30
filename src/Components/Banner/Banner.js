657038ba6165cb77ecec030d4892b0409ec16be6
mnaofficial <mna.official92@gmail.com>
2022-05-01 01:22:54 +0600

    Banner page setup complete

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,9 +1,49 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import banner1 from '../../Images/Banner/banner1.png'
+import banner2 from '../../Images/Banner/banner2.png'
+import banner3 from '../../Images/Banner/banner3.png'
 
 const Banner = () => {
     return (
         <div>
-            <h1>Banner</h1>
+            <Carousel>
+                <Carousel.Item interval={1000}>
+                    <img
+                    style={{height: '400px'}}
+                        className="d-block w-100"
+                        src={banner1}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>MNA Cars Warehouse</h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={500}>
+                    <img
+                    style={{height: '400px'}}
+
+                        className="d-block w-100"
+                        src={banner2}
+                        alt="Second slide"
+                    />
+                    <Carousel.Caption>
+                    <h3>MNA Cars Warehouse</h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    style={{height: '400px'}}
+
+                        className="d-block w-100"
+                        src={banner3}
+                        alt="Third slide"
+                    />
+                    <Carousel.Caption>
+                    <h3>MNA Cars Warehouse</h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
         </div>
     );
 };
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
+import Footer from '../Footer/Footer';
 
 const Home = () => {
     return (
         <div>
-            
+            <Banner></Banner>
             <h2>This is Home</h2>
+
+            <Footer></Footer>
         </div>
     );
 };
