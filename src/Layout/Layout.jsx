99cefb9beec233fd4ae665912ad6aa7a4bedcf54
mnaofficialbd <mna.official92@gmail.com>
2025-06-11 23:29:24 +0600

    Layout setup

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,11 +1,14 @@
 import Footer from "../Components/Shared/Footer";
 import Header from "../Components/Shared/Header/Header";
+import Routers from "../Routes/Routers";
 
 const Layout = () => {
   return (
     <>
       <Header />
-      <main>{/* <Routers /> */}</main>
+      <main>
+        <Routers />
+      </main>
       <Footer />
     </>
   );
---
+++ src/Routes/Routers.jsx
@@ -0,0 +1,7 @@
+import { Routes } from "react-router-dom";
+
+const Routers = () => {
+  return <Routes></Routes>;
+};
+
+export default Routers;
