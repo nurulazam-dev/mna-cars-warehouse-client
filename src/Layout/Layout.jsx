d413abd2467a9b9246dd7cb97291866c5b0c45e3
mnaofficialbd <mna.official92@gmail.com>
2025-06-11 23:44:56 +0600

    Layout and App update

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,15 +1,22 @@
+import { useLocation } from "react-router-dom";
 import Footer from "../Components/Shared/Footer";
 import Header from "../Components/Shared/Header/Header";
 import Routers from "../Routes/Routers";
 
 const Layout = () => {
+  const location = useLocation();
+
+  const hideFooter = ["/dashboard", "/login", "/register"].some((path) =>
+    location.pathname.startsWith(path)
+  );
+
   return (
     <>
       <Header />
       <main>
         <Routers />
       </main>
-      <Footer />
+      {!hideFooter && <Footer />}
     </>
   );
 };
