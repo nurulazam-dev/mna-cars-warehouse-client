e1ad8b3224872d66e1b5ad6b7b1cefc519270c3f
mnaofficialbd <mna.official92@gmail.com>
2025-06-11 23:23:02 +0600

    Layout create and setup

---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,14 @@
+import Footer from "../Components/Shared/Footer";
+import Header from "../Components/Shared/Header/Header";
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <main>{/* <Routers /> */}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
