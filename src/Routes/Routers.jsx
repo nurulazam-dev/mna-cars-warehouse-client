b9c0711e7420e5b930bb79ca6c90928891f9bbb5
mnaofficialbd <mna.official92@gmail.com>
2025-06-11 23:39:55 +0600

    Routers create and setup

--- src/Routes/Routers.jsx
+++ src/Routes/Routers.jsx
@@ -1,7 +1,133 @@
-import { Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import RequireAuth from "./RequireAuth";
+import Home from "../Pages/Home";
+import UpdateProfile from "../Components/Dashboard/UpdateProfile";
+import DeleteItem from "../Components/Dashboard/Admin/DeleteItem";
+import Dashboard from "../Pages/Dashboard";
+import Blogs from "../Pages/Blogs";
+import Items from "../Pages/Items/Items";
+import ItemDetails from "../Pages/Items/ItemDetails";
+import Booking from "../Pages/Items/Booking";
+import ContactDealer from "../Pages/Items/ContactDealer";
+import Register from "../Pages/Register";
+import Login from "../Pages/Login";
+import Overview from "../Components/Dashboard/Overview";
+import ManageItems from "../Components/Dashboard/Admin/ManageItems";
+import ManageBookings from "../Components/Dashboard/Admin/ManageBookings";
+import ManageUsers from "../Components/Dashboard/Admin/ManageUsers";
+import AddItem from "../Components/Dashboard/Admin/AddItem";
+import UpdateItem from "../Components/Dashboard/Admin/UpdateItem";
+import MyBookingItems from "../Components/Dashboard/User/MyBookingItems";
+import AboutUs from "../Components/About/AboutUs";
+import AboutDeveloper from "../Components/About/AboutDeveloper";
+import NotFound from "../Pages/NotFound";
 
 const Routers = () => {
-  return <Routes></Routes>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/home" element={<Home />}></Route>
+      <Route path="/blogs" element={<Blogs />}></Route>
+      <Route path="/items" element={<Items />}></Route>
+      <Route path="/items/:id" element={<ItemDetails />}></Route>
+      <Route path="/booking" element={<Booking />} />
+      <Route path="/contact-dealer" element={<ContactDealer />} />
+      <Route path="/register" element={<Register />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+
+      <Route path="/dashboard" element={<Dashboard />}></Route>
+      <Route
+        path="dashboard"
+        element={
+          <RequireAuth>
+            <Dashboard />
+          </RequireAuth>
+        }
+      >
+        <Route index element={<Overview />} />
+
+        {/*  <Route
+            path="ManageInventories"
+            element={
+              <RequireAdmin>
+                <ManageInventories />
+              </RequireAdmin>
+            }
+          /> */}
+        <Route
+          path="Manage-items"
+          element={
+            <RequireAuth>
+              <ManageItems />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="Manage-bookings"
+          element={
+            <RequireAuth>
+              <ManageBookings />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="Manage-users"
+          element={
+            <RequireAuth>
+              <ManageUsers />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="add-item"
+          element={
+            <RequireAuth>
+              <AddItem />
+            </RequireAuth>
+          }
+        ></Route>
+        <Route
+          path="update-item/:id"
+          element={
+            <RequireAuth>
+              <UpdateItem />
+            </RequireAuth>
+          }
+        ></Route>
+        <Route
+          path="delete-item/:id"
+          element={
+            <RequireAuth>
+              <DeleteItem />
+            </RequireAuth>
+          }
+        ></Route>
+        <Route
+          path="update-profile"
+          element={
+            <RequireAuth>
+              <UpdateProfile />
+            </RequireAuth>
+          }
+        ></Route>
+
+        <Route
+          path="my-booking-items"
+          element={
+            <RequireAuth>
+              <MyBookingItems />
+            </RequireAuth>
+          }
+        ></Route>
+      </Route>
+      {/* ============= */}
+
+      <Route path="/about-us" element={<AboutUs />}></Route>
+      <Route path="/about-developer" element={<AboutDeveloper />}></Route>
+
+      <Route path="*" element={<NotFound />}></Route>
+    </Routes>
+  );
 };
 
 export default Routers;
