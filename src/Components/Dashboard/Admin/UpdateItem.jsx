0ff8f79398c1703481053b14390ea7e879553e16
mnaofficialbd <mna.official92@gmail.com>
2025-06-11 23:00:18 +0600

    update-item component update

--- src/Components/Dashboard/Admin/UpdateItem.jsx
+++ src/Components/Dashboard/Admin/UpdateItem.jsx
@@ -1,23 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-
 const UpdateItem = () => {
-    const {itemId} = useParams();
-    // const [item]=useItem(itemId)
-
-    const [item, setItem] = useState({});
-    useEffect(() => {
-        const url = `https://salty-spire-70121.herokuapp.com/item/update/${itemId}`;
-        fetch(url)
-        .then(res=>res.json())
-        .then(data=>setItem(data))
-    }, [])
-
-    return (
-        <div>
-            <h2 className='text-center text-primary'>Update Item {item.name}</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2 className="text-center text-primary">Update Item </h2>
+    </div>
+  );
 };
 
 export default UpdateItem;
---
+++ src/Components/Dashboard/UpdateProfile.jsx
@@ -0,0 +1,9 @@
+const UpdateProfile = () => {
+  return (
+    <div>
+      <h2 className="text-center text-primary mt-3">Update Profile</h2>
+    </div>
+  );
+};
+
+export default UpdateProfile;
