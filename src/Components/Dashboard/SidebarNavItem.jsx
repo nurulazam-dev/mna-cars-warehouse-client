fdbd2ee6abce576a37c5ba54a45644f22fcc8f3b
mnaofficialbd <mna.official92@gmail.com>
2025-06-15 23:55:12 +0600

    dashboard SidebarNavItem component create

---
+++ src/Components/Dashboard/SidebarNavItem.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const SidebarNavItem = ({ to, icon, label, active }) => {
+  return (
+    <Nav.Link
+      as={Link}
+      to={to}
+      className={`sidebar-item ${active ? "active" : ""}`}
+    >
+      <i className={`bi ${icon} nav-icon`}></i>
+      <span className="nav-label">{label}</span>
+    </Nav.Link>
+  );
+};
+
+export default SidebarNavItem;
