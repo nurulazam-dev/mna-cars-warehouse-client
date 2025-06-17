84a23133e6fae9f86451b15d8eeafa47d89763fb
mnaofficialbd <mna.official92@gmail.com>
2025-06-17 10:23:11 +0600

    myWishlist update

--- src/Components/Dashboard/User/MyWishlist.jsx
+++ src/Components/Dashboard/User/MyWishlist.jsx
@@ -9,34 +9,39 @@ const MyWishlist = () => {
     token
   );
 
-  if (loading) return <Loader />;
-
   return (
-    <div className="container">
+    <section className="container">
       <h2 className="text-center mb-2 text-primary fw-bold">My Wishlist</h2>
-      {wishlist?.length === 0 ? (
-        <p className="text-center text-danger">No items in wishlist.</p>
-      ) : (
-        <div className="row">
-          {wishlist?.map((item) => (
-            <div className="col-md-4 mb-3" key={item?._id}>
-              <div className="card">
-                <div className="card-body">
-                  <h5>{item?.title}</h5>
-                  <p>{item?.description}</p>
-                  <button
-                    className="btn btn-danger btn-sm"
-                    onClick={() => deleteWishlistItem(item?._id)}
-                  >
-                    Remove
-                  </button>
+
+      {loading && <Loader />}
+
+      {!loading && (
+        <div>
+          {wishlist?.length === 0 ? (
+            <p className="text-center text-danger">No items in wishlist.</p>
+          ) : (
+            <div className="row">
+              {wishlist?.map((item) => (
+                <div className="col-md-4 mb-3" key={item?._id}>
+                  <div className="card">
+                    <div className="card-body">
+                      <h5>{item?.title}</h5>
+                      <p>{item?.description}</p>
+                      <button
+                        className="btn btn-danger btn-sm"
+                        onClick={() => deleteWishlistItem(item?._id)}
+                      >
+                        Remove
+                      </button>
+                    </div>
+                  </div>
                 </div>
-              </div>
+              ))}
             </div>
-          ))}
+          )}
         </div>
       )}
-    </div>
+    </section>
   );
 };
 
