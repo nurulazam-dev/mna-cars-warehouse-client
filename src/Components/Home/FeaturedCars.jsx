92ba87567a4e2035447a222cbf8486254b14cce8
mnaofficialbd <mna.official92@gmail.com>
2025-06-21 19:52:17 +0600

    FeaturedCars update

--- src/Components/Home/FeaturedCars.jsx
+++ src/Components/Home/FeaturedCars.jsx
@@ -201,13 +201,6 @@ const FeaturedCars = () => {
           ))}
         </div>
       </div>
-      {/* =====================
-            Animate.css CDN
-     ===================== */}
-      <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
-      />
     </section>
   );
 };
