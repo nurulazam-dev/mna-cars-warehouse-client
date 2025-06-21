e2ae7d7ffe3d792024436833ab41f0539548aef4
mnaofficialbd <mna.official92@gmail.com>
2025-06-21 19:51:46 +0600

    ContactInfo update

--- src/Components/Home/ContactInfo.jsx
+++ src/Components/Home/ContactInfo.jsx
@@ -156,11 +156,6 @@ const ContactInfo = () => {
           </div>
         </div>
       </div>
-      {/* Animate.css CDN */}
-      <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
-      />
     </section>
   );
 };
