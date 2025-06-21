f6227f6fc3affc258028e1b0ce2852a31cd69aba
mnaofficialbd <mna.official92@gmail.com>
2025-06-21 19:56:41 +0600

    ContactDealer update

--- src/Pages/Items/ContactDealer.js
+++ src/Pages/Items/ContactDealer.js
@@ -173,11 +173,6 @@ const ContactDealer = () => {
           </div>
         </div>
       )}
-      {/* Animate.css CDN */}
-      <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
-      />
     </section>
   );
 };
