f814fa34181862a8d51968e524675a2ed9adfa01
mnaofficialbd <mna.official92@gmail.com>
2025-07-12 22:47:29 +0600

    404 page  update

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,12 +1,14 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import notFound from "../assets/images/NotFound/404.png";
 
 const NotFound = () => {
   return (
-    <section className="container animate__animated animate__fadeInUp w-100 mx-auto text-center">
+    <section
+      className="container animate__animated animate__fadeIn text-center"
+      style={{ minHeight: "90vh" }}
+    >
       <div>
-        <img className=" w-75" src={notFound} alt="" />
+        <img className=" w-75 mb-4" src={notFound} alt="" />
       </div>
       <Link to="/">
         <button className="btn btn-primary w-25 fs-5">Go Home</button>
